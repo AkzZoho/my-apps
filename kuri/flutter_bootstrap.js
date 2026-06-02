e5f906d6a71a98243cc4485f995c6ab67800e58e
AkzZoho <AkzZoho@users.noreply.github.com>
2026-06-02 21:31:14 +0000

    deploy: 9ee89d96a342c36089cd98183bce9d7ee48360d1

--- kuri/flutter_bootstrap.js
+++ kuri/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1919979456"
+    serviceWorkerVersion: "1372351300"
   }
 });
