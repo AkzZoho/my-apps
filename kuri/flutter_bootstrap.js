d821ce8fcfe3f1b9f2776515c3dc1d0b1bdfb336
AkzZoho <AkzZoho@users.noreply.github.com>
2026-06-03 12:34:01 +0000

    deploy: 857616b4c33921e9312d0544ad318267d220d7e3

--- kuri/flutter_bootstrap.js
+++ kuri/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3335925037" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2696503466" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
