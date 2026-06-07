1b315be57fc87a81d7f3213f083d363f1bd4472c
AkzZoho <AkzZoho@users.noreply.github.com>
2026-06-07 18:22:33 +0000

    deploy: 88dcc5a72fd5fd52da8ce4b16cabcd5204e3d5a4

--- committee/flutter_bootstrap.js
+++ committee/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2040783321" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1805725737" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
